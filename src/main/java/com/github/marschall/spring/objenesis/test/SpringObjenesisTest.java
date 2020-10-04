e187c4679d20d4ef796f7680a143c2e44c1be970
Philippe Marschall <philippe.marschall@gmail.com>
2020-10-04 14:41:38 +0200

    call SpringObjenesis directly

--- src/main/java/com/github/marschall/spring/objenesis/test/SpringObjenesisTest.java
+++ src/main/java/com/github/marschall/spring/objenesis/test/SpringObjenesisTest.java
@@ -10,19 +10,24 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.objenesis.SpringObjenesis;
 
 public class SpringObjenesisTest extends HttpServlet {
 
-  private ConfigurableApplicationContext applicationContext;
+  private volatile ConfigurableApplicationContext applicationContext;
+  private volatile SpringObjenesis springObjenesis;
 
   @Override
   public void init() throws ServletException {
     this.applicationContext = new AnnotationConfigApplicationContext(ApplicationConfiguration.class);
+    this.springObjenesis = new SpringObjenesis();
   }
 
   @Override
   public void destroy() {
     this.applicationContext.close();
+    this.applicationContext = null;
+    this.springObjenesis = null;
   }
 
   @Override
@@ -30,6 +35,8 @@ public class SpringObjenesisTest extends HttpServlet {
     response.setCharacterEncoding("US-ASCII");
     PrintWriter writer = response.getWriter();
     writer.write("ok:" + this.applicationContext.getBean(ApplicationBean.class));
+    writer.write("\r\n");
+    writer.write("ok:" + this.springObjenesis.newInstance(ApplicationBean.class));
   }
 
 }
